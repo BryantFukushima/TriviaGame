435f23d000bd09a8fcbad7fccdd09cd38a86c032
Bryant Fukushima <bmfukushima94@gmail.com>
2018-06-30 01:15:35 -0700

    added 7 questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,9 +7,13 @@ gameWindow.hide();
 finWindow.hide();
 
 var questions = [
-	{ id: 'one', question: 'What is my Name?', opOne: 'George', opTwo: 'Bryant', opThree: 'Kaiyer', opFour: 'Ikaika' },
-	{ id: 'two', question: 'What has legs?', opOne: 'snake', opTwo: 'fish', opThree: 'worm', opFour: 'dog'},
-	{ id: 'three', question: 'What island is Honolulu located on?', opOne: 'Oahu', opTwo: 'Maui', opThree: "Kaho'olawe", opFour: 'Niihau'},
+	{ id: 'one', question: 'In the 1989 film "Back to the Future: Part II", what day in the future did Marty McFly and Doc Brown travel to?', opOne: 'December 21, 2012', opTwo: 'August 7, 2020', opThree: 'October 21, 2015', opFour: 'April 14, 2010' },
+	{ id: 'two', question: "What are the colors of the Irish Flag?", opOne: 'Red and White', opTwo: 'Red, White, and Green', opThree: 'Red, Black, and Yellow', opFour: 'Green, White, and Orange'},
+	{ id: 'three', question: 'What is depicted in the Starbucks logo?', opOne: 'Siren, the two tailed Mermaid', opTwo: 'Cecilia, the Mother of Coffee', opThree: 'Aurelia, Queen Starbucks', opFour: 'Amphitrite, goddess-Queen of the Sea'},
+	{ id: 'four', question: 'What is the hidden symbol in the FedEx logo?', opOne: 'A truck', opTwo: 'An arrow', opThree: 'A plane', opFour: 'There is no hidden symbol'},
+	{ id: 'five', question: 'What is Harry Potters Patronus?', opOne: 'Phoenix', opTwo: 'Stag', opThree: "Doe", opFour: 'Horse'},
+	{ id: 'six', question: 'Who is on the front of the golden dollar coin?', opOne: 'Thomas Jefferson', opTwo: 'John F. Kennedy', opThree: "Grover Cleveland", opFour: 'Sacagawea'},
+	{ id: 'seven', question: 'How many colors are in a rainbow?', opOne: '6', opTwo: '7', opThree: "8", opFour: '9'},
 ];
 
 var questionsDone = [];
@@ -40,7 +44,7 @@ function mainTimer() {
 		$('#timeLimit').text(time);
 		gameWindow.hide();
 		finWindow.show();
-		finWindow.text('Incorrect, correct answer is ' + $('[data-correct=true]').text());
+		finWindow.text('Time is up. Correct answer is ' + $('[data-correct=true]').text());
 		intTimer = setInterval(intervalTimer , 1000);
 	}
 }
@@ -62,7 +66,7 @@ function pauGame() {
 	var results = $('<p>');
 	results.text('Correct: ' + wins + ' Incorrect: ' + losses);
 	finWindow.append(results);
-	var startOver = $('<span>');
+	var startOver = $('<p>');
 	startOver.addClass('restart');
 	startOver.text('Start Over?')
 	finWindow.append(startOver);
@@ -78,7 +82,7 @@ function questionGen() {
 		pauGame();
 	} else {
 
-		time = 15;
+		time = 30;
 		$('#timeLimit').text(time);
 		timer = setInterval(mainTimer, 1000);
 
@@ -95,13 +99,20 @@ function questionGen() {
 		}
 		
 		if (questions[randomQues].id == 'one') {
-			$('#opTwo').attr('data-correct' , true);
+			$('#opThree').attr('data-correct' , true);
 		} else if (questions[randomQues].id == 'two') {
 			$('#opFour').attr('data-correct' , true);
 		} else if (questions[randomQues].id == 'three') {
 			$('#opOne').attr('data-correct' , true);
+		} else if (questions[randomQues].id == 'four') {
+			$('#opTwo').attr('data-correct' , true);
+		} else if (questions[randomQues].id == 'five') {
+			$('#opTwo').attr('data-correct' , true);
+		} else if (questions[randomQues].id == 'six') {
+			$('#opFour').attr('data-correct' , true);
+		} else if (questions[randomQues].id == 'seven') {
+			$('#opTwo').attr('data-correct' , true);
 		}
-
 
 		$('#opOne').text(questions[randomQues].opOne);
 		$('#opTwo').text(questions[randomQues].opTwo);
